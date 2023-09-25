00956fbfc2f2f9bb9340b0eafef5ca8b95d7b64a
natejmaster@gmail.com <natejmaster@gmail.com>
2023-09-24 20:47:58 -0500

    Added code so comments appear

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,6 +1,11 @@
 module.exports = {
   format_date: (date) => {
+    if (!date) return ""; // Handle the case when date is undefined
+
+    // Ensure date is a Date object
+    const formattedDate = date instanceof Date ? date : new Date(date);
+
     // Format date as MM/DD/YYYY
-    return date.toLocaleDateString();
-  }
+    return formattedDate.toLocaleDateString();
+  },
 };
